27665a5c99513b7d054bb46754c083e79480c3a4
Be-bo <robfiker@gmail.com>
2023-11-02 15:14:19 -0700

    Submission version

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.scss";
 import { useState } from "react";
 import guru from "./image/guru.jpeg";
+import axios from "axios";
 
 function App() {
   const [inputValue, setInputValue] = useState("");
@@ -17,6 +18,8 @@ function App() {
 
   const [selectedValue, setSelectedValue] = useState("0");
   const [roleValue, setRoleValue] = useState("");
+
+  const [responseData, setresponseData] = useState([]);
 
   const handleChange = (event) => {
     setInputValue(event.target.value);
@@ -75,7 +78,7 @@ function App() {
     const data = {
       skills: valueList,
       work_experience: experienceList,
-      interest: interestsList,
+      interests: interestsList,
       education: educationList,
       desired_role: roleValue,
       response_type: parseInt(selectedValue, 10),
@@ -86,143 +89,150 @@ function App() {
   };
   const submitDatatoServer = async (finaldata) => {
     console.log(finaldata);
-    // const response = await axios.post("http://localhost:8080",finaldata);
-    // console.log(response.data);
+    const response = await axios.post("http://localhost:8080/chat", finaldata);
+    console.log(response.data.message.content);
+    setresponseData(response.data.message.content);
   };
 
   return (
-    <div className="outerbox">
-      <div className="App">
-        <div className="img">
-          <img src={guru} alt="guru image" />
+    <>
+      <div className="outerbox">
+        <div className="App">
+          <div className="img">
+            <img src={guru} alt="guru image" />
+          </div>
+          <h1>CAREER GURU</h1>
+
+          <form onSubmit={handleSubmitForm}>
+            <section>
+              <label>Skills</label>
+              <input
+                type="text"
+                value={inputValue}
+                onChange={handleChange}
+                onKeyPress={handleEnterKey}
+              />
+              <ul className="list">
+                {valueList.map((value, index) => (
+                  <li className="list-box" key={index}>
+                    {value}
+                  </li>
+                ))}
+              </ul>
+            </section>
+
+            <section>
+              <label>Work Experience</label>
+              <input
+                type="text"
+                value={experienceValue}
+                onChange={handleExperienceChange}
+                onKeyPress={handleExperienceEnterKey}
+              />
+              <ul className="list">
+                {experienceList.map((value, index) => (
+                  <li className="list-box" key={index}>
+                    {value}
+                  </li>
+                ))}
+              </ul>
+            </section>
+
+            <section>
+              <label>Education</label>
+              <input
+                type="text"
+                value={educationValue}
+                onChange={handleEducationChange}
+                onKeyPress={handleEducationEnterKey}
+              />
+              <ul className="list">
+                {educationList.map((value, index) => (
+                  <li className="list-box" key={index}>
+                    {value}
+                  </li>
+                ))}
+              </ul>
+            </section>
+
+            <section>
+              <label>Interests</label>
+              <input
+                type="text"
+                value={interestsValue}
+                onChange={handleInterestsChange}
+                onKeyPress={handleInterestsEnterKey}
+              />
+              <ul className="list">
+                {interestsList.map((value, index) => (
+                  <li className="list-box" key={index}>
+                    {value}
+                  </li>
+                ))}
+              </ul>
+            </section>
+
+            <section>
+              <label>Desired Role</label>
+              <input
+                type="text"
+                name="role"
+                value={roleValue}
+                onChange={handleRoleChange}
+              />{" "}
+            </section>
+
+            <section>
+              <h3>Type of Response:</h3>
+              <div>
+                <input
+                  type="radio"
+                  name="response"
+                  value="0"
+                  checked={selectedValue === "0"}
+                  onChange={handleRadioChange}
+                />
+                <label for="skillsToLearn">
+                  What skills should I learn next?
+                </label>
+              </div>
+              <div>
+                <input
+                  type="radio"
+                  name="response"
+                  value="1"
+                  checked={selectedValue === "1"}
+                  onChange={handleRadioChange}
+                />
+                <label for="skillsMissing">
+                  What crucial skills and experience am I missing?
+                </label>
+              </div>
+              <div>
+                <input
+                  type="radio"
+                  name="response"
+                  value="2"
+                  checked={selectedValue === "2"}
+                  onChange={handleRadioChange}
+                />
+                <label for="skillsForJob">
+                  What do I need to do to maximize my chances of getting a job
+                  right now?
+                </label>
+              </div>
+              <p>Selected value: {selectedValue}</p>
+            </section>
+
+            <button>Submit</button>
+          </form>
+
+          <div>
+            <p>{responseData}</p>
+          </div>
         </div>
-        <h1>CAREER GURU</h1>
-
-        <form onSubmit={handleSubmitForm}>
-          <section>
-            <label>Skills</label>
-            <input
-              type="text"
-              value={inputValue}
-              onChange={handleChange}
-              onKeyPress={handleEnterKey}
-            />
-            <ul className="list">
-              {valueList.map((value, index) => (
-                <li className="list-box" key={index}>
-                  {value}
-                </li>
-              ))}
-            </ul>
-          </section>
-
-          <section>
-            <label>Work Experience</label>
-            <input
-              type="text"
-              value={experienceValue}
-              onChange={handleExperienceChange}
-              onKeyPress={handleExperienceEnterKey}
-            />
-            <ul className="list">
-              {experienceList.map((value, index) => (
-                <li className="list-box" key={index}>
-                  {value}
-                </li>
-              ))}
-            </ul>
-          </section>
-
-          <section>
-            <label>Education</label>
-            <input
-              type="text"
-              value={educationValue}
-              onChange={handleEducationChange}
-              onKeyPress={handleEducationEnterKey}
-            />
-            <ul className="list">
-              {educationList.map((value, index) => (
-                <li className="list-box" key={index}>
-                  {value}
-                </li>
-              ))}
-            </ul>
-          </section>
-
-          <section>
-            <label>Interests</label>
-            <input
-              type="text"
-              value={interestsValue}
-              onChange={handleInterestsChange}
-              onKeyPress={handleInterestsEnterKey}
-            />
-            <ul className="list">
-              {interestsList.map((value, index) => (
-                <li className="list-box" key={index}>
-                  {value}
-                </li>
-              ))}
-            </ul>
-          </section>
-
-          <section>
-            <label>Desired Role</label>
-            <input
-              type="text"
-              name="role"
-              value={roleValue}
-              onChange={handleRoleChange}
-            />{" "}
-          </section>
-
-          <section>
-            <h3>Type of Response:</h3>
-            <div>
-              <input
-                type="radio"
-                name="response"
-                value="0"
-                checked={selectedValue === "0"}
-                onChange={handleRadioChange}
-              />
-              <label for="skillsToLearn">
-                What skills should I learn next?
-              </label>
-            </div>
-            <div>
-              <input
-                type="radio"
-                name="response"
-                value="1"
-                checked={selectedValue === "1"}
-                onChange={handleRadioChange}
-              />
-              <label for="skillsMissing">
-                What crucial skills and experience am I missing?
-              </label>
-            </div>
-            <div>
-              <input
-                type="radio"
-                name="response"
-                value="2"
-                checked={selectedValue === "2"}
-                onChange={handleRadioChange}
-              />
-              <label for="skillsForJob">
-                What do I need to do to maximize my chances of getting a job
-                right now?
-              </label>
-            </div>
-            <p>Selected value: {selectedValue}</p>
-          </section>
-
-          <button>Submit</button>
-        </form>
       </div>
-    </div>
+    </>
   );
 }
 
